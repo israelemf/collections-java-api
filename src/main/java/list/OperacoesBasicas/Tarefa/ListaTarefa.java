518281ed3c57b0984c5e1c3edff5d8e423aaaa32
Israel Meloso Filho <israelemf@outlook.com>
2023-08-24 23:01:00 -0300

    Interface List | Operação Básica: Lista de Tarefas

---
+++ src/main/java/list/OperacoesBasicas/Tarefa/ListaTarefa.java
@@ -0,0 +1,41 @@
+package main.java.list.OperacoesBasicas.Tarefa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    // Atributo
+    private List<Tarefa> tarefas;
+
+    public ListaTarefa() {
+        this.tarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefas.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasRemover = new ArrayList<>();
+
+        for (Tarefa tarefa : tarefas) {
+            if (tarefa.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasRemover.add(tarefa);
+            }
+        }
+        tarefas.removeAll(tarefasRemover);
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefas.size();
+    }
+
+    public StringBuilder obterDescricoesTarefas() {
+        StringBuilder descricaoTarefas = new StringBuilder("Descrições");
+
+        for (Tarefa tarefa : tarefas) {
+            descricaoTarefas.append("\n").append(tarefa);
+        }
+        return descricaoTarefas;
+    }
+}
---
+++ src/main/java/list/OperacoesBasicas/Tarefa/Tarefa.java
@@ -0,0 +1,15 @@
+package main.java.list.OperacoesBasicas.Tarefa;
+
+public class Tarefa {
+    // Atributo
+    private String descricao;
+
+    // Construtor
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
