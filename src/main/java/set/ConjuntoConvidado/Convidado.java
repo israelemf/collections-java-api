1f5a265de723a5698f9bbe8c0efb3b18798232fc
Israel Meloso Filho <israelemf@outlook.com>
2023-09-03 21:56:59 -0300

    Interface Set  | Operação Básica: Teste da classe ConjuntoConvidados

--- src/main/java/set/ConjuntoConvidado/Convidado.java
+++ src/main/java/set/ConjuntoConvidado/Convidado.java
@@ -1,5 +1,7 @@
 package main.java.set.ConjuntoConvidado;
 
+import java.util.Objects;
+
 public class Convidado {
     private String nome;
     private int codigoConvite;
@@ -15,6 +17,19 @@ public class Convidado {
 
     public int getCodigoConvite() {
         return codigoConvite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Convidado convidado = (Convidado) o;
+        return codigoConvite == convidado.codigoConvite;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigoConvite);
     }
 
     @Override
---
+++ src/main/java/set/ConjuntoConvidado/Main.java
@@ -0,0 +1,21 @@
+package main.java.set.ConjuntoConvidado;
+
+public class Main {
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+        conjuntoConvidados.adicionarConvidado("Convidado1", 1);
+        conjuntoConvidados.adicionarConvidado("Convidado2", 2);
+        conjuntoConvidados.adicionarConvidado("Convidado3", 3);
+        conjuntoConvidados.adicionarConvidado("Convidado4", 4);
+        conjuntoConvidados.adicionarConvidado("Convidado5", 4);
+
+        conjuntoConvidados.exibirConvidados();
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set de convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(2);
+
+        conjuntoConvidados.exibirConvidados();
+    }
+}
