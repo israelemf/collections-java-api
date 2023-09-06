626a5ed8274cd6fe6c709e5c33dd1a622fa34130
Israel Meloso Filho <israelemf@outlook.com>
2023-09-06 00:06:56 -0300

    Interface Map  | Operação Básica: Teste da classe AgendaContatos

---
+++ src/main/java/map/OperacoesBasicas/AgendaContatos/Main.java
@@ -0,0 +1,24 @@
+package main.java.map.OperacoesBasicas.AgendaContatos;
+
+public class Main {
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Israel", "41995642868");
+        agendaContatos.adicionarContato("Israel", "41995642868");
+        agendaContatos.adicionarContato("Jennifer", "4154546465");
+        agendaContatos.adicionarContato("Amanda", "4164566666");
+        agendaContatos.adicionarContato("Israel", "64564564");
+        agendaContatos.adicionarContato("Luca", "41995642868");
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.removerContato("Israel");
+
+        agendaContatos.exibirContatos();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Jennifer"));
+
+
+    }
+}
